2c7b7566afb8c9c3a2cb072a41f795be119fa8a6
Arjan de Ruijter <rra@mboutrecht.nl>
2020-03-18 07:22:01 +0100

    4.2 Maken van een registratieformulier - het formulier

--- register.php
+++ register.php
@@ -1,1 +1,19 @@
-registreer
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-12 col-sm-6">
+      <form action="./index.php?content=register_script" method="post">
+        <div class="form-group">
+          <label for="inputEmail">Vul hier uw e-mailadres in:</label>
+          <input name="email" type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp">
+          <small id="emailHelp" class="form-text text-muted">Uw privacy is gegarandeerd...</small>
+        </div>
+        
+        <button type="submit" class="btn btn-warning btn-lg btn-block">Registreer</button>
+      </form>
+    </div>
+    <div class="col-12 col-sm-6">
+      <img src="./img/kersen.jpg" alt="kersen" class="w-75 mx-auto d-block">
+    </div>
+  </div>
+
+</div>
