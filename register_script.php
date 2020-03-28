02e30e5610f7f739fe052880ba1ecea02723e067
Arjan de Ruijter <rra@mboutrecht.nl>
2020-03-28 13:44:58 +0100

    mk_password_hash function gerealiseerd

--- register_script.php
+++ register_script.php
@@ -14,19 +14,10 @@
     if (mysqli_num_rows($result)) {
       header("Location: ./index.php?content=message&alert=emailexists");
     } else {
-      $mut = microtime();
-
-      $time = explode(" ", $mut);
-
-      $password = $time[1] * $time[0] * 1000000;
-
-      $password_hash = password_hash($password, PASSWORD_BCRYPT);
-
-      $onehour = mktime(1,0 ,0, 1, 1, 1970);
- 
-      $d = date("d-m-Y", ($time[1] + $onehour));
-
-      $t = date("H:i:s", ($time[1] + $onehour));
+      // De functie mk_password_hash_from_microtime() maakt een password hash,
+      // haalt de tijd en datum op op basis van de php-functie microtime() 
+      // en geeft dit terug in $array
+      $array = mk_password_hash_from_microtime();      
       
       $sql = "INSERT INTO `register` (`id`,
                                       `email`,
@@ -34,7 +25,7 @@
                                       `userrole`)
               VALUES                 (NULL,
                                       '$email',
-                                      '$password_hash',
+                                      '{$array["password_hash"]}',
                                       'customer')";
 
       if (mysqli_query($conn, $sql)) {
